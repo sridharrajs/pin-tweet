1421ba5ed7a84157bf4cab2da797b45d13151c00
sridharrajs <sridhar@aghenam.com>
2022-12-08 18:17:36 +0530

    stripping excess spaces and setting 2 space

--- cli.js
+++ cli.js
@@ -10,22 +10,22 @@ environment.assertAll({ dotenv, services: ['pinboard'] });
 const urls = [];
 
 for (let url of urls) {
-    const articleUrl = removeTrackers(url);
-    crawlerService.fetchTitle(articleUrl).then((title) => {
-        return pinboardService.addUrl({ articleUrl, title })
-    }).then(body => {
-        if (body.result_code !== 'done') {
-            console.log('Failure', {
-                body,
-                articleUrl,
-                status: info.status,
-                statusText: info.statusText
-            });
-        }
-    }).catch(err => {
-        console.log('Error', {
-            status: err.status,
-            statusText: err.statusText
-        });
+  const articleUrl = removeTrackers(url);
+  crawlerService.fetchTitle(articleUrl).then((title) => {
+    return pinboardService.addUrl({ articleUrl, title })
+  }).then(body => {
+    if (body.result_code !== 'done') {
+      console.log('Failure', {
+        body,
+        articleUrl,
+        status: info.status,
+        statusText: info.statusText
+      });
+    }
+  }).catch(err => {
+    console.log('Error', {
+      status: err.status,
+      statusText: err.statusText
     });
+  });
 }
--- service/crawler-service.js
+++ service/crawler-service.js
@@ -5,26 +5,26 @@ const cheerio = require('cheerio');
  * Attempts to fetch title of a page,
  * incase of failure uses URL of the page to
  * be the title
- * @param {*} url 
- * @returns 
+ * @param {*} url
+ * @returns
  */
 function fetchTitle(url) {
-    return new Promise((resolve, reject) => {
-        request(url, function (error, response, body) {
-            if (error || response.statusCode !== 200) {
-                return resolve(encodeURI(url));
-            }
+  return new Promise((resolve, reject) => {
+    request(url, function (error, response, body) {
+      if (error || response.statusCode !== 200) {
+        return resolve(encodeURI(url));
+      }
 
-            var $ = cheerio.load(body);
+      var $ = cheerio.load(body);
 
-            // if the title doesnt exist use URL
-            title = resolve($("head > title").text().trim() || url);
+      // if the title doesnt exist use URL
+      title = resolve($("head > title").text().trim() || url);
 
-            // for cases if the title has unescapeable characters
-            // we can use encode URL
-            return encodeURI(fetchTitle);
-        });
+      // for cases if the title has unescapeable characters
+      // we can use encode URL
+      return encodeURI(fetchTitle);
     });
+  });
 }
 
 module.exports = { fetchTitle };
