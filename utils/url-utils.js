687b4b3ea0e4feebbe92d90fbbfb60ce48ae552a
sridharrajs <sridhar@aghenam.com>
2022-12-08 18:49:13 +0530

    move trackers to configs

--- utils/url-utils.js
+++ utils/url-utils.js
@@ -1,14 +1,11 @@
 const normalizeUrl = require('normalize-url');
 
+const { trackers = [] } = require('../rules.json');
+
 function removeTrackers(url) {
   return normalizeUrl(url, {
     stripHash: true,
-    removeQueryParameters: [
-      'ref',
-      'utm_campaign',
-      'utm_medium',
-      'utm_source'
-    ]
+    removeQueryParameters: trackers
   });
 }
 
