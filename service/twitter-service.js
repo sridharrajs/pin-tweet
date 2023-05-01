76eeb17447a1ce5b74acdd1c418c45a620f74b0e
sridharrajs <sridhar@aghenam.com>
2023-05-01 09:09:15 +0530

    tech: migrate to twitter official sdk

--- service/twitter-service.js
+++ service/twitter-service.js
@@ -1,55 +1,49 @@
 'use strict';
 
-const Twitter = require('twitter');
-const { promisify } = require('util');
+const { TwitterApi } = require('twitter-api-v2');
 
 const {
-  TWITTER_SCREEN_NAME: SCREEN_NAME,
+  TWITTER_ID,
   TWITTER_CONSUMER_KEY,
   TWITTER_CONSUMER_SECRET,
   TWITTER_ACCESS_TOKEN_KEY,
   TWITTER_ACCESS_TOKEN_KEY_SECRET,
 } = process.env;
 
-const client = new Twitter({
-  consumer_key: TWITTER_CONSUMER_KEY,
-  consumer_secret: TWITTER_CONSUMER_SECRET,
-  access_token_key: TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: TWITTER_ACCESS_TOKEN_KEY_SECRET,
+const client = new TwitterApi({
+  appKey: TWITTER_CONSUMER_KEY,
+  appSecret: TWITTER_CONSUMER_SECRET,
+  accessToken: TWITTER_ACCESS_TOKEN_KEY,
+  accessSecret: TWITTER_ACCESS_TOKEN_KEY_SECRET,
 });
 
-/**
- * API for Twitter service
- * listLikes = https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/get-favorites-list
- * listLikes = https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/post-favorites-destroy
- */
-
 function listLikes() {
-  return promisify(client.get.bind(client))('favorites/list', {
-    screen_name: SCREEN_NAME,
-    tweet_mode: 'extended',
-    count: 5 // 20 can be the MAX
-  }).then(tweets => {
-    return tweets.map(tweet => {
+  return client.v2.get(`users/${TWITTER_ID}/liked_tweets`, {
+    expansions: ['referenced_tweets.id.author_id'],
+    'user.fields': ['username', 'description', 'name'],
+  }).then((tweets) => {
+    const { data = [], includes: { users: authors = [] } = {} } = tweets;
+    return data?.map(tweet => {
+      const { id, author_id: authorId, text } = tweet;
+      const authorUserName = authors.find(user => user.id === authorId)?.username ?? 'twitter';
       return {
-        id: tweet.id_str,
-        title: tweet.full_text,
-        articleUrl: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`,
+        id,
+        title: text,
+        articleUrl: `https://twitter.com/${authorUserName}/status/${id}`,
         entities: tweet.entities,
-        tweetBy: tweet.user.screen_name,
-      };
-    });
+        tweetBy: authorUserName,
+      }
+    })
   }).catch(err => {
     console.error('Error in twitter-service:listLikes()', err);
     return [];
   });
-}
-
+}``
 
 function destroyLikes(id) {
-  return promisify(client.post.bind(client))('favorites/destroy', {
-    id: `${id}`
-  });
+  return client.v2.unlike(TWITTER_ID, id).catch(err => {
+    console.error('Error in twitter-service:destroyLikes()', err);
+  })
 }
 
 module.exports = {
